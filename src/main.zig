b5a0f27d30ae7c9933b78a624434336760760684
lapiks <dhenin.julien@gmail.com>
2025-07-15 11:42:02 +0200

    The PPM Image Format

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,28 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const image_width = 256;
+    const image_height = 256;
 
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    try stdout.print("P3\n{} {}\n255\n", .{ image_width, image_height });
+
+    for (0..image_height) |j| {
+        for (0..image_width) |i| {
+            const r = @as(f32, @floatFromInt(i)) / @as(f32, image_width - 1);
+            const g = @as(f32, @floatFromInt(j)) / @as(f32, image_height - 1);
+            const b = 0.0;
+
+            const ir = @as(u8, @intFromFloat(255.999 * r));
+            const ig = @as(u8, @intFromFloat(255.999 * g));
+            const ib = @as(u8, @intFromFloat(255.999 * b));
+
+            try stdout.print("{} {} {}\n", .{ ir, ig, ib });
+        }
+    }
 
     try bw.flush();
 }
